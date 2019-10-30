b34fdb602c98710d1db7a3926708fa9c69557748
LQH <liqihai1987@gmail>
2019-10-30 09:37:09 +0800

    添加验证

--- src/TLLibs/Ctrl/AuthCtrl.php
+++ src/TLLibs/Ctrl/AuthCtrl.php
@@ -6,6 +6,8 @@ namespace TLLibs\Ctrl;
 
 use Faker\Provider\Uuid;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Input;
+use TLLibs\Auth\WXAuth;
 use TLLibs\Common\ApiCtrl;
 use TLLibs\Format\LoginFormat;
 use TLLibs\Models\User;
@@ -21,7 +23,9 @@ class AuthCtrl extends ApiCtrl
         $this->formatTransfer = new LoginFormat();
     }
 
-    public function accountLogin($acct, $pwd){
+    public function accountLogin(){
+        $acct = Input::get('account',null);
+        $pwd = Input::get('pwd',null);
         $user = User::where(function ($query) use ($acct, $pwd) {
             $query->where('phone', $acct)
                 ->orWhere('email', $acct);
@@ -37,8 +41,15 @@ class AuthCtrl extends ApiCtrl
         return $this->toJsonItem($user);
     }
 
-    public function weChatLogin($wxOpenId)
+    public function weChatLogin()
     {
+        $code = Input::get('code',null);
+        $wxId = $_SERVER['wx_key'];
+        $wxSecret = $_SERVER['wx_secret'];
+        $wxOpenId = (new WXAuth($wxId,$wxSecret))->decryptData($code);
+        if(empty($wxOpenId)){
+            abort(404);
+        }
         $user = User::firstOrCreate(['wx_id'=>$wxOpenId]);
         $user->token = Uuid::uuid();
         $user->save();
